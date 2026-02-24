fff31e3dc301d7e7b4694fb09073231c497b9576
Clashnewbme <amoryjamesmonson@icloud.com>
2026-02-24 09:30:36 +0800

    files added

---
+++ Fresco-Project/go.mod
@@ -0,0 +1,5 @@
+module example.com/golangapp
+
+go 1.25.1
+
+require github.com/webview/webview_go v0.0.0-20240831120633-6173450d4dd6
---
+++ Fresco-Project/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/webview/webview_go"
+)
+
+func main() {
+	w := webview.New(true)
+	defer w.Destroy()
+
+	w.SetTitle("Fresco Browser")
+	w.SetSize(1000, 700, webview.HintNone)
+
+	w.Navigate("https://setup-delta.vercel.app")
+
+	w.Run()
+}
